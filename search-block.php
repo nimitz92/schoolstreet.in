3efbd79431940194e9590ae7f9a8850ad8d4f138
Himanshu Singh <hs.thegreat@gmail.com>
2012-02-23 06:08:11 +0530

    refactored

---
+++ search-block.php
@@ -0,0 +1,31 @@
+<div class="sidebar">
+				
+<div class="side-search">
+		<h2>Search</h2>
+		<form class="search-form">
+			<table>
+				<tr>
+					<td>
+						<select class="search-selectbox" name="search-for-options">
+						<option value="search-school">Search for School</option>
+						<option value="search-student">Search for Student</option>
+						</select>
+						<input type="text" name="date" id="date" />
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input class="search-textbox city-input" type="textbox" name="city" value="City" />
+					</td>
+				</tr>
+				<tr>								
+					<td>
+						<input class="search-textbox school-input" type="textbox" name="school" value="Name of the School" />
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
+
+
+</div>
